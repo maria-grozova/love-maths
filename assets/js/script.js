eb21f2657341c897884981b913f0b36d6e6d2501
Maria Grozova <marija.grozova@gmail.com>
2024-06-23 15:48:58 +0000

    start addition game mechanics in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
 });
 
 /**
@@ -21,10 +23,17 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
     //creates random numbers 1 to 25
     let num1 = Math.ceil(Math.random() * 25);
     let num2 = Math.ceil(Math.random() * 25);
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert("you did a bad");
+        throw("where's the rest of the code");
+    }
 }
 
 function checkAnswer() {
@@ -43,8 +52,10 @@ function incrementWrong() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").innerText = operand1;
+    document.getElementById("operand2").innerText = operand2;
+    document.getElementById("operator").innerText = "+";
 }
 
 function displaySubtractionQuestion() {
