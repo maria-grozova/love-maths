057580a42f00bd3740fba13fbf18ee7ad66de5c2
Maria Grozova <marija.grozova@gmail.com>
2024-06-23 13:51:27 +0000

    add basic game structure in js

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrong() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractionQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
