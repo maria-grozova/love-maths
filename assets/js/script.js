6c7f4c14525d67172c21289c8069db30a8ee7ea2
Maria Grozova <marija.grozova@gmail.com>
2024-06-24 07:37:45 +0000

    add functions for incrementing score tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,8 +48,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrong()
     }
 
     runGame(calculatedAnswer[1]);
@@ -73,12 +75,21 @@ function calculateCorrectAnswer() {
         throw `Unimplemented thing ${operator}. Aborting!`;
     }
 }
-function incrementScore() {
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Gets the current number of wrong answers from the DOM and increments it by 1
+ */
 function incrementWrong() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
